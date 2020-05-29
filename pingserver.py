daca4419b5ed8c67ef08654f53dcab8fb48d2e9e
Kei Ohnuma <linuxkay@users.noreply.github.com>
2020-05-30 07:28:51 +0900

    added pingserver.py

---
+++ pingserver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python
+#This python script is to check server is alive or not by ping
+import time
+import os
+hostname = "PUT TARGET IP or HOSTNAME"
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+#while True:
+if response == 0:
+  time.sleep(3)
+  print hostname, 'is up!'
+else:
+  os.system("mpg123 /home/user/Music/beep_down.mp3")
+  time.sleep(1.2)
+  print hostname, 'is down!'
