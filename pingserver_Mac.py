45b1f81b2fdf618f2c5533974656bfba26409f15
Ohnuma <ohnuma@3c0630138fe6.ant.amazon.com>
2022-08-09 20:57:05 +0900

    Mac version added

---
+++ pingserver_Mac.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env/python3
+#This python script is to check server is alive or not by ping
+import time
+import os
+hostname = "Hostname"
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+#while True:
+if response == 0:
+  time.sleep(3)
+  os.system("afplay sounds/beep-07.mp3")
+  print(hostname, 'is Alive')
+else:
+  os.system("afplay sounds/beep_down.mp3")
+  time.sleep(1.2)
+  print(hostname, 'is down!')
