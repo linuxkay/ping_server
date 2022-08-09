3f34caa92eaaa51d5445727a5601e401c334007d
linuxkay <sccck010@gmail.com>
2022-08-09 21:26:34 +0900

    Updated pingserver.py

--- pingserver.py
+++ pingserver.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env/python
+#!/usr/bin/env/python3
 #This python script is to check server is alive or not by ping
 import time
 import os
-hostname = "PUT TARGET IP or HOSTNAME"
-response = os.system("ping -c 1 " + hostname)
+hostname = input("Enter HostName/IP: ")
 
-#and then check the response...
-#while True:
-if response == 0:
-  time.sleep(3)
-  os.system("mpg123 sounds/beep-07.mp3")
-  print hostname, 'is up!'
-else:
-  os.system("mpg123 sounds/beep_down.mp3")
-  time.sleep(1.2)
-  print hostname, 'is down!'
+while True:
+  response = os.system("ping -c 1 " + hostname)
+  if response == 0:
+      time.sleep(3)
+      os.system("mpg123 sounds/beep-07.mp3")
+      print(hostname, 'is Alive')
+  else:
+      os.system("mpg123 sounds/beep_down.mp3")
+      time.sleep(1.2)
+      print(hostname, 'is down!')
